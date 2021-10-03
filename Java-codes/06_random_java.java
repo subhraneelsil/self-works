eaa501b0f6932c55ca4dab434db1a6ba44d93924
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-10-03 23:01:59 +0530

    Add files via upload

---
+++ Java-codes/06_random_java.java
@@ -0,0 +1,13 @@
+/*
+Created by Subhraneel Sil
+On 03.10.2021
+
+***Generate a random number in Java***
+ */
+package com.company;
+import java.lang.Math;
+public class random_java {
+    public static void main(String args[]) {
+        System.out.println(Math.random());
+    }
+}
