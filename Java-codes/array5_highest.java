8b3f175114d88dcf18bd83deca756d3a5b4d92f9
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-10-19 23:33:08 +0530

    Add files via upload

---
+++ Java-codes/array5_highest.java
@@ -0,0 +1,28 @@
+/*
+Created by Subhraneel Sil
+On 19.10.2021
+***Largest element of the array***
+ */
+package com.company;
+
+import java.util.Scanner;
+
+public class array5_highest {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int ar[]=new int[5];
+        int temp;
+        for(int i=0;i<5;i++) {
+            ar[i] = sc.nextInt(i);
+        }
+        int max = ar[0];
+
+        for (int i = 0; i <5; i++) {
+
+            if(ar[i] >max)
+                max = ar[i];
+        }
+        System.out.println("Largest element present in given array: " + max);
+    }
+}
