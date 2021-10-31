64306b1c18e9c4129253f21e45a5213b5ffb45b5
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-10-31 21:19:09 +0530

    Add files via upload

---
+++ Java-codes/modulus.java
@@ -0,0 +1,24 @@
+/*
+Created by  Subhraneel Sil
+Modulus operation
+ */
+package com.company;
+
+import java.util.Scanner;
+
+public class modulus {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println(a);
+        }
+        else
+        {
+            System.out.println(-1*a);
+        }
+    }
+}
