6e3b294cf48c1b594761ff6ec1319d8ade1a566e
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-11-11 23:39:03 +0530

    Add files via upload

---
+++ Java-codes/object_creation.java
@@ -0,0 +1,18 @@
+/*
+Created by Subhraneel Sil
+ */
+package com.company;
+public class object_creation
+{
+    void show()
+    {
+        System.out.println("Welcome to javaTpoint");
+    }
+    public static void main(String[] args)
+    {
+
+        object_creation obj = new object_creation();
+
+        obj.show();
+    }
+}
