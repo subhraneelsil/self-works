6ba778ee80c7ec6ed18bd02a21d301c6a48cad3b
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-11-25 23:38:50 +0530

    Add files via upload

---
+++ Java-codes/object_comparison.java
@@ -0,0 +1,15 @@
+/*
+Created by Subhraneel Sil
+ */
+package com.company;
+
+public class object_comparison {
+        public static void main(String[] args)
+        {
+            Double x = new Double(123.45555);
+            Long y = new Long(9887544);
+            System.out.println("Objects are not equal, hence it returns " + x.equals(y));
+            System.out.println("Objects are equal, hence it returns " + x.equals(123.45555));
+        }
+    }
+
