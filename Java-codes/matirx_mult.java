154768af55ecb3e4245a95240868cf471aed3d2f
subhraneelsil <91549270+subhraneelsil@users.noreply.github.com>
2021-12-03 22:34:09 +0530

    Add files via upload

---
+++ Java-codes/matirx_mult.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+class matrix_mult{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a[][]=new int[3][3];
+        int b[][]=new int[3][3];
+        int c[][]=new int[3][3];
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                a[i][j]=sc.nextInt();
+                b[i][j]= sc.nextInt();
+            }
+        }
+        for(int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][j] * b[i][j];
+            }
+        }
+        for(int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(c[i][j]);
+            }
+        }
+    }
+}
